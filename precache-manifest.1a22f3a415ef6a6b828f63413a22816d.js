ade3495286e59119af0a516d5d51130f92fb9b5e
Fan W <fwu@Fans-MBP.fritz.box>
2020-04-20 14:25:31 +0200

    Updates

---
+++ precache-manifest.1a22f3a415ef6a6b828f63413a22816d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b74b134c933c0a082e23ea61475fc66",
+    "url": "/fantocode/index.html"
+  },
+  {
+    "revision": "152b1674f53a463f3501",
+    "url": "/fantocode/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "189bd125021d0dda9190",
+    "url": "/fantocode/static/js/2.bd11f694.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fantocode/static/js/2.bd11f694.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "152b1674f53a463f3501",
+    "url": "/fantocode/static/js/main.9f4c6f90.chunk.js"
+  },
+  {
+    "revision": "b9a5f4ff6c4bfb8756b1",
+    "url": "/fantocode/static/js/runtime-main.39e1e6c1.js"
+  }
+]);
